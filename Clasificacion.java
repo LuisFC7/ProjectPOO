31e5cbed2c7b5232836121e48a25345954db25f8
dejannyy <61927515+dejannyy@users.noreply.github.com>
2020-03-08 22:24:23 -0600

    Add files via upload

---
+++ Clasificacion.java
@@ -0,0 +1,43 @@
+
+public class Clasificacion extends Registration
+{
+	public void perros()
+	{
+		System.out.println("---------------------PERROS---------------------");
+		System.out.println("Perros:");
+		System.out.println("Pastor aleman:");
+		System.out.println("Schnauzer");
+		System.out.println("Rotweiler");
+		System.out.println("San Bernardo");
+		System.out.println("Terrier");
+		System.out.println("Yorkshire terrier");
+		System.out.println("Dachshund");
+		System.out.println("Husky siberiano");
+		System.out.println("Akita");
+		System.out.println("Xoloitzcuintle");
+		System.out.println("Sabueso");
+		System.out.println("Dalmata");
+		System.out.println("Labrador retriever");
+		System.out.println("Gran danés");
+		
+		
+	}
+	public void gatos()
+	{
+		System.out.println("---------------------GATOS---------------------");
+		System.out.println("Savannah");
+		System.out.println("Bosque de Noruega");
+		System.out.println("Siberiano");
+		System.out.println("Persa");
+		System.out.println("Europeo");
+		System.out.println("Egipcio");
+		System.out.println("Ocicat");
+		System.out.println("Toyger");
+		System.out.println("Burmés");
+		System.out.println("Americano");
+		System.out.println("Abisinio");
+		System.out.println("Esfinge");
+		System.out.println("PeterBald");
+	}
+
+}
